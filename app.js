e3ba245057167015a5af6f2b71c7525b2b9e23c6
Andrew Olson <olson@cshl.edu>
2014-03-18 11:42:52 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,118 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3001);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+//////////////////////////////////////
+////// data and metadata routes //////
+//////////////////////////////////////
+var db = require('monk')('localhost/test'),
+    genomes = db.get('genomes'),
+    users = db.get('users'),
+    fastbit = db.get('fastbit');
+
+app.get('/genome', function (req, res, next) {
+    // return a list of genomes from mongodb
+    // available properties are {genomeID, species, version, taxon, description, sequences}
+    // download everything but sequences.
+    // client will lazy load the sequences and lengths for each requested genome
+    genomes.find({}, 'genomeID species version', function(err,docs) {
+        res.json(docs);
+    });
+});
+app.get('/genome/:id', function (req,res,next) {
+    // return info on this genome 
+    genomes.findOne({genomeID:+req.params.id}, '-sequences', function(err,doc) {
+        res.json(doc);
+    });
+});
+app.get('/genome/:id/chromosomes', function (req,res,next) {
+   // return chromosomes and lengths for requested genome 
+   genomes.findOne({genomeID:+req.params.id}, 'sequences', function(err,doc) {
+       res.json(doc.sequences);
+   });
+});
+
+app.get('/genome/:id/datasets', function (req,res,next) {
+    // return the datasets annotated on requested genome
+    fastbit.find({genomeID:+req.params.id}, function(err,docs) {
+        res.json(docs);
+    });
+});
+var fb = {
+    hostname: 'localhost',
+    port: 3000
+};
+app.get('/data/:set', function (req,res) { 
+    // return this data set (check permissions?)
+    // /:set/describe
+    var options = {
+        hostname: fb.hostname,
+        port: fb.port,
+        path: '/'+req.params.set+'/describe'
+    };
+    http.get(options, function(response) {
+        var body = '';
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
+        response.on('end', function() {
+            res.send(body);
+        });
+    }).on('error', function(e) {
+        throw(e);
+    });
+});
+
+app.get('/data/:set/:command', function (req,res,next) { // don't web-cache this because of the permissions check the node-fastbit service caches too, so no worries.
+    // check permissions 
+    // proxy the request through the node-fastbit service
+    var options = {
+        hostname: fb.hostname,
+        port: fb.port,
+        path: req.url.replace(/^\/data/,'')
+    };
+    http.get(options, function(response) {
+        var body = '';
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
+        response.on('end', function() {
+            res.send(body);
+        });
+    }).on('error', function(e) {
+        throw(e);
+    });
+});
---
+++ public/javascripts/test.js
@@ -0,0 +1,150 @@
+// some local variables
+var genomeLength = 0; // nucleotides
+var gapLength = Math.PI / 90;    // radians
+var tracks = new Object();
+var chrLengths = new Object();
+var chrStarts = new Object();
+var nChrs = 0;
+var axisWidth = 2;
+var radiansPerBase;
+var trackWidth = 20;
+var trackSpacing = 5;
+
+var canvas;
+var ctx;
+var x;
+var y;
+var radius;
+
+function initViz() {
+    canvas = document.getElementById('drawhere');
+    ctx = canvas.getContext('2d');
+    x = canvas.width / 2;
+    y = x;
+    radius = 0.92*x;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    var startAngle = 0;
+    ctx.lineWidth = axisWidth;
+    ctx.strokeStyle = 'black';
+
+    radiansPerBase = (2*Math.PI - nChrs*gapLength) / genomeLength; 
+    for(var chr in chrLengths) {
+        chrStarts[chr] = startAngle;
+        ctx.beginPath();
+        var arcLength = chrLengths[chr]*radiansPerBase;
+        ctx.arc(x,y,radius,startAngle,startAngle + arcLength, false);
+        ctx.stroke();
+        var mid = startAngle + arcLength/2;
+        startAngle += arcLength + gapLength;
+        ctx.fillText(chr,x+1.05*radius*Math.cos(mid),y+1.05*radius*Math.sin(mid));
+    }
+}
+
+function addTrack(setID) {
+    // calculate the radius for this track
+    var r = radius;
+    for(var t in tracks) {
+        r -= trackSpacing;
+        r -= trackWidth;
+    }
+    r -= trackWidth;
+    tracks[setID] = new Object();
+    ctx.lineWidth = trackWidth;
+    // ctx.beginPath();
+    // ctx.arc(x,y,r,0,2*Math.PI,false);
+    // ctx.stroke();
+    var max=0;
+    var chrCounts = new Object();
+    var stride = Math.floor(genomeLength/(Math.PI*r));
+    var remaining = nChrs;
+    for(var chr in chrLengths) {
+        (function(c){
+            $.getJSON("/data/"
+            +setID+"/dist?column=pos&part="
+            +c+"&begin=0&end="
+            +chrLengths[c]+"&stride="+stride, function(res) {
+                chrCounts[c] = [];
+                for(var i=0;i<res.bounds.length;i++) {
+                    chrCounts[c][i] = res.counts[i];
+                    if (res.counts[i] > max) max = res.counts[i];
+                }
+                remaining--;
+                if (remaining === 0) {
+                    console.log(max);
+                    for(var chr2 in chrLengths) {
+                        var startAngle = chrStarts[chr2];
+                        var arcLength = stride*radiansPerBase;
+                        for(var i=0;i<chrCounts[chr2].length;i++) {
+                            ctx.beginPath();
+                            ctx.arc(x,y,r,startAngle,startAngle + arcLength, false);
+                            var norm = chrCounts[chr2][i]/max;
+                            ctx.strokeStyle = 'rgba(0,0,0,'+norm+')';
+                            ctx.stroke();
+                            startAngle += arcLength;
+                        }
+                    }
+                }
+            });
+        })(chr)
+    }
+}
+
+function changeGenome(genomeID) {
+    var dataset = document.getElementById('dataset');
+    $(dataset).html('');
+    // lookup data sets
+    var s = $('<select id="dataSel"/>');
+    s.append($('<option/>').attr('value',0).text('Select dataset'));
+    $.getJSON("/genome/"+genomeID+"/datasets", function(res) {
+        $.each(res, function(key, value) {
+            s.append($('<option/>').attr('value',value.setID)
+            .text(value.description));
+        });
+        $(dataset).append(s);
+        $(dataset).append($('<button/>').text('Add')
+        .click(function() {
+            var dataSel = document.getElementById('dataSel');
+            var setID = $(dataSel).val();
+            if (! tracks.hasOwnProperty(setID)) addTrack(setID);
+            if (setID == 0) {
+                myimage = new Image();
+                myimage.onload = function() {
+                    ctx.drawImage(myimage, x-300, y-200);
+                }
+                myimage.src = 'http://images.pictureshunt.com/pics/p/porky_pig_thats_all_folks-5172.jpg';
+            }
+        }));
+    });
+    // lookup chromosome lengths
+    $.getJSON("/genome/"+genomeID+"/chromosomes", function(res) {
+        genomeLength=0;
+        // empty tracks and chrLengths
+        tracks = {};
+        chrLengths = {};
+        nChrs=0;
+        for(var chr in res) {
+            chrLengths[chr] = res[chr];
+            genomeLength += res[chr];
+            nChrs++;
+        }
+        initViz();
+    });
+}
+
+function init() {
+    var genome = document.getElementById('genome');
+    var s = $('<select/>');
+    s.change(function() {
+        changeGenome($(this).val());
+    });
+    s.append($('<option/>').attr('value',0).text('Select genome'));
+    $.getJSON( "/genome", function(res) {
+        $.each(res, function(key, value) {
+            s.append($('<option/>').attr('value',value.genomeID)
+            .text(value.species + ' -- ' + value.version));
+        });
+        $(genome).append(s);
+    });
+}
